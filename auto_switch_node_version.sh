da35e44d70cfaf448cf6d8a4b62e777a208cb605
Sean Becker <sbecker@fubo.tv>
2022-06-24 16:39:11 -0500

    add auto switch node version

---
+++ auto_switch_node_version.sh
@@ -0,0 +1,23 @@
+# add the following below nvm initialization in ~/.zshrc
+autoload -U add-zsh-hook
+load-nvmrc() {
+  [[ -a .nvmrc ]] || return
+
+  local node_version="$(nvm version --silent)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
